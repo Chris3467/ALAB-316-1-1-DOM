874b7f097ee3f193856f08f3c7a758d941a1957b
Chris3467 <chris.collado815@gmail.com>
2024-11-06 18:16:03 -0500

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-
-
 // Nav Bar
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
-
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
 
 // Part 1 ============================>
 const mainEl = document.querySelector("main");
@@ -15,23 +35,42 @@ mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
 mainEl.classList.add("flex-ctr");
 
-
-
 // Part 2 =============================>
 const topMenuEl = document.querySelector("#top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
-
-
 // Part 3 =============================>
-menuLinks.forEach(link => {
-    const newEl = document.createElement("a");
-    newEl.href = link.href;
-    newEl.textContent = link.text;
-    topMenuEl.append(newEl);
-}) 
+menuLinks.forEach((link) => {
+  const newEl = document.createElement("a");
+  newEl.href = link.href;
+  newEl.textContent = link.text;
+  topMenuEl.append(newEl);
+});
 
+// Second Part of the DOM lab ===========>
+// Part 1
 
+const subMenuEl = document.getElementById("sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
 
+// Part 2 ================================>
+
+// Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+const topMenuLinks = topMenuEl.querySelectorAll("a");
+console.log(topMenuLinks);
+
+// Attach a delegated 'click' event listener to topMenuEl.
+topMenuEl.addEventListener("click", handler);
+
+// The first line of code of the event listener function should call the event object's preventDefault() method.
+function handler(evt) {
+  if (evt.target.localName !== a) {
+    return;
+  } else evt.preventDefault();
+}
