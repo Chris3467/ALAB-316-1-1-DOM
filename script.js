e96210fcc5401a90635bd03b5181809399265798
Chris3467 <chris.collado815@gmail.com>
2024-11-07 21:15:02 -0500

    finished

--- script.js
+++ script.js
@@ -74,3 +74,60 @@ function handler(evt) {
     return;
   } else evt.preventDefault();
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  for (let i =0; i < topMenuLinks.length; i++){
+    const clicked = topMenuLinks[i]
+     console.log(clicked);
+
+    clicked.addEventListener("click", function(evt){
+        if(!evt.target.matches('a'))return
+        console.log(evt.target);
+        evt.target.classList.toggle('active') 
+        topMenuLinks.forEach(link=>{
+          console.log(link.subLinks);
+          if(link!==evt.target){
+             link.classList.remove('active')
+          }
+          const clickedLink = menuLinks.find((link)=>link.text==evt.target.textContent)
+          console.log(clickedLink.subLinks);
+          if(evt.target.classList.contains('active')&& evt.target.innerHTML!== "about"){
+            subMenuEl.style.top="100%"
+            buildSubMenu(clickedLink.subLinks)
+          }else{
+            subMenuEl.style.top="0%"
+          }
+        })
+          function buildSubMenu(subLinks){
+            console.log(subLinks);
+            console.log(subMenuEl);
+            subMenuEl.innerHTML =""
+            subLinks.forEach(link=>{
+              const a = document.createElement('a')
+              a.setAttribute('href', link.href)
+              a.textContent= link.text
+              subMenuEl.appendChild(a)
+            })
+          }
+          subMenuEl.addEventListener("click", function(evt){
+            evt.preventDefault()
+            if(!evt.target.matches('a')){
+              return;
+            } 
+            console.log(evt.target);
+            subMenuEl.style.top ="0%"
+            topMenuLinks.forEach(link=>{
+              link.classList.remove('active')
+            })
+            h1.textContent =`${evt.target.textContent}`
+          })
+   
+        
+        console.log(this);
+        
+  })
+}
+  
+});
